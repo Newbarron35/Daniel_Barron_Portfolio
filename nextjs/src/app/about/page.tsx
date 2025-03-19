1f281c417bde86ea9fa595acc2a85e36b85cf099
thiccboydannyboy <newbarron35@gmail.com>
2025-03-19 10:35:20 -0700

    mmm

---
+++ nextjs/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Header from "@/components/header";
+
+export default function Aboutpage() {
+  return (
+    <div className={styles.page}>
+      <Header color="blue" title="What you getting at with the book, scripts, spit that shit out, man!" />
+   </div>
+  );
+}
